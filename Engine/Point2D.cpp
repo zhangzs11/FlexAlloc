e0c5161922d21884cd40b325c1f586724b54af67
zhangzs11 <143010540+zhangzs11@users.noreply.github.com>
2023-09-17 16:51:06 -0600

    create Point2D class in Engine

---
+++ Engine/Point2D.cpp
@@ -0,0 +1,36 @@
+#include <stdexcept>
+#include "Point2D.h"
+
+Point2D Point2D::operator+(const Point2D& other) const {
+	return Point2D(x + other.x, y + other.y);
+}
+Point2D Point2D::operator-(const Point2D& other) const {
+	return Point2D(x - other.x, y - other.y);
+}
+float Point2D::operator*(const Point2D& other) const {
+	return x * other.x + y * other.y;
+}
+Point2D Point2D::operator*(float scalar) const {
+	return Point2D(x * scalar, y * scalar);
+}
+Point2D Point2D::operator/(float scalar) const {
+	if (scalar == 0.0f) {
+		throw std::runtime_error("Division by zero!");
+	}
+	return Point2D(x / scalar, y / scalar);
+}
+bool Point2D::operator==(const Point2D& other) const {
+	return x == other.x && y == other.y;
+}
+bool Point2D::operator!=(const Point2D& other) const {
+	return !(*this == other);
+}
+float Point2D::distance(const Point2D& a, const Point2D& b) {
+	float dx = a.x - b.x;
+	float dy = a.y - b.y;
+	return sqrt(dx * dx + dy * dy);
+}
+void Point2D::move(const Point2D& velocity, float time) {
+	x += velocity.x * time;
+	y += velocity.y * time;
+}
--- Engine/Point2D.h
+++ Engine/Point2D.h
@@ -1,1 +1,22 @@
 #pragma once
+class Point2D {
+private:
+	float x, y;
+public:
+	Point2D() :x(0), y(0) {}
+	Point2D(float x, float y) :x(x), y(y) {}
+	float getX() const { return x; }
+	float getY() const { return y; }
+	void setX(float x) { this->x = x; }
+	void setY(float y) { this->y = y; }
+
+	Point2D operator+(const Point2D& other) const;
+	Point2D operator-(const Point2D& other) const;
+	float operator*(const Point2D& other) const;
+	Point2D operator*(float scalar) const;
+	Point2D operator/(float scalar) const;
+	bool operator==(const Point2D& other) const;
+	bool operator!=(const Point2D& other) const;
+	static float distance(const Point2D& a, const Point2D& b);
+	void move(const Point2D& velocity, float time);
+};
